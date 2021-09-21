40ab67dc8b34464b28c66d74938da48b373ef2da
mosaab-rady <mosp.rady@gmail.com>
2021-09-21 21:49:33 +0200

    added address and order history

--- src/component/Addresses.js
+++ src/component/Addresses.js
@@ -2,16 +2,99 @@ import React from 'react';
 
 export default function Addresses() {
   return (
-    <div className='account__address'>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
-      <h3>addresses</h3>
+    <div className='account--body addresses'>
+      <h2 className='account--body__header'>address book</h2>
+      <div className='account--body__addresses'>
+        <h2 className='account--body__addresses__header'>add address</h2>
+        <form className='form'>
+          <div className='addresses__form'>
+            <div className='form__group'>
+              <label htmlFor='firstname' className='form__group__label'>
+                first name
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                placeholder='first name'
+                name='firstname'
+                required
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='lastname' className='form__group__label'>
+                last name
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                placeholder='last name'
+                required
+                name='lastname'
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='address1' className='form__group__label'>
+                address 1
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                placeholder='address 1'
+                required
+                name='address1'
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='address2' className='form__group__label'>
+                address 2
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                placeholder='address 2'
+                name='address2'
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='city' className='form__group__label'>
+                city
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                placeholder='city'
+                required
+                name='city'
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='country' className='form__group__label'>
+                country
+              </label>
+              <input
+                type='country'
+                className='form__group__in'
+                name='country'
+                required
+                placeholder='country'
+              />
+            </div>
+            <div className='form__group'>
+              <label htmlFor='zip' className='form__group__label'>
+                zip
+              </label>
+              <input
+                type='text'
+                className='form__group__in'
+                name='zip'
+                required
+                placeholder='zip'
+              />
+            </div>
+          </div>
+          <button className='address__btn'>save</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/component/OrderHistory.js
+++ src/component/OrderHistory.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { request } from '../js/axios';
 
 export default function OrderHistory() {
-  const [orders, setOrders] = useState('');
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       const res = await request('GET', '/api/v1/shopping/myshoppings');
       if (res) {
+        console.log(res);
         if (res.data.status === 'success') {
           setOrders(res.data.data.shoppings);
         }
@@ -17,14 +18,33 @@ export default function OrderHistory() {
   }, []);
 
   return (
-    <div>
-      <h1>order history</h1>
-      <h1>order history</h1>
-      <h1>order history</h1>
-      <h1>order history</h1>
-      <h1>order history</h1>
-      <h1>order history</h1>
-      <h1>order history</h1>
+    <div className='account--body'>
+      <h2 className='account--body__header'>order history</h2>
+      <div className='account--body__orders'>
+        <table className='account--body__orders__table'>
+          <thead>
+            <tr>
+              <th>order</th>
+              <th>date</th>
+              <th>status</th>
+              <th>total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((item, i) => (
+              <tr key={i}>
+                <td>
+                  {item.product.color} {item.product.name}{' '}
+                  <span>{item.size}</span>
+                </td>
+                <td>{item.createdAt.split('T')[0]}</td>
+                <td>{item.paid ? 'paid' : 'not paid'}</td>
+                <td>${item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
